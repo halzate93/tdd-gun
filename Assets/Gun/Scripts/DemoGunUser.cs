01f70b43ac952702ed4dd8d72f14af4dea50a097
Héctor C. Alzate <halzate93@gmail.com>
2016-02-19 13:58:23 -0500

    Finished Gun demo.

---
+++ Assets/Gun/Scripts/DemoGunUser.cs
@@ -0,0 +1,18 @@
+﻿using Brainz.Demo.TDD.Gun;
+using UnityEngine;
+using Zenject;
+
+namespace Brainz.Demo.TDD
+{
+	public class DemoGunUser : MonoBehaviour
+	{
+		[Inject]
+		private IGunBehaviour gun;
+
+		private void Update()
+		{
+			if (Input.GetKeyDown(KeyCode.Space))
+				gun.Shoot();
+		}
+	}
+}
